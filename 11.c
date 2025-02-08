ec5fb78f7abc5f0a3fb01b1ff8a35589f76cf453
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-08 11:34:19 +0600

    Delete 11.c

--- 11.c
+++
@@ -1,9 +0,0 @@
-//What would be the values of the array arr in the following program?
-#include <stdio.h>
-int main()
-{
-    int i, arr[3] = {0, 1, 2};
-    for(i = 1; i < 4; i++)
-        arr[arr[arr[3-i]]] = i-1;
-    return 0;
-}
