e9940feba0bf85dda76398c84619cecac5e5593b
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-02-12 18:27:59 +0600

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i, arr[5];
+    arr[0] = 100;
+    arr[1] = 101;
+    arr[2] = 102;
+    arr[3] = 103;
+    arr[4] = 104;
+    for(i = 0; i < 5; i++)
+        printf("%d\n", arr[i]);
+    return 0;
+}
